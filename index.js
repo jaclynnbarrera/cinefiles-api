819724c4a8b662da0f2c7f4611c103c0d06230fb
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-05-24 12:57:59 -0400

    adds post route

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+app.post("/images", async (req, res) => {
+  try {
+    const { description, color, frame, url } = req.body;
+    const newImage = await pool.query(
+      "INSERT INTO image (description, color, frame, url) VALUES ($1, $2, $3, $4) RETURNING *",
+      [description, color, frame, url]
+    );
+    res.json(newImage.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
